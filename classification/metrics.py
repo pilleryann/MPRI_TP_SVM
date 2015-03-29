c777f5c55d0b74176b87eaa045be1d9df18db6c9
pilleryann <piller.yann@gmail.com>
2015-03-29 15:45:45 +0200

    - Implement confusion matrix normalized

--- classification/metrics.py
+++ classification/metrics.py
@@ -1,6 +1,19 @@
 from sklearn.metrics.metrics import confusion_matrix, classification_report
 import pylab as pl
+import numpy as np
+import matplotlib.pyplot as plt
 
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    target_names =['0','1','2','3','4','5','6','7','8','9']
+    tick_marks = np.arange(len(target_names))
+    plt.xticks(tick_marks, target_names, rotation=45)
+    plt.yticks(tick_marks, target_names)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 def show_confusion_matrix(y_true, y_predicted, title=''):
     """
@@ -10,7 +23,9 @@ def show_confusion_matrix(y_true, y_predicted, title=''):
     # TODO: show confusion matrix plot
     # Compute confusion matrix
     cm = confusion_matrix(y_true, y_predicted)
-    
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    plt.figure()
+    plot_confusion_matrix(cm_normalized, title='Normalized confusion matrix')
     print(cm)
     
     # Show confusion matrix in a separate window
